6a86f564a7d9b9eeeb4a4b153864fbb066f910fa
Lucas544875 <takayanagi691@gmail.com>
2024-05-06 01:14:34 +0900

    [update] 検索ロジックの実行 #21

--- src/class/KanjiRestore.ts
+++ src/class/KanjiRestore.ts
@@ -1,28 +1,58 @@
 import * as data from "../data/UCSkanji-tree.json";
+import * as data_reverse from "../data/UCSkanji-tree-reverse.json";
 
 enum OperationState {
   INIT,
   TOP,
-  BOTTOM,
+  BOTTOM
 }
 
 type KanjiWithIds = {
   kanji: string,
-  ids: string
+  ids: string[]
 }
 
 export class KanjiRestore {
   private kanjiTree: any;
+  private kanjiTreeReverse: any;
 
   constructor() {
     this.kanjiTree = data;
+    this.kanjiTreeReverse = data_reverse;
   }
 
-  public restore(subIds: string, way:OperationState): KanjiWithIds {
-    let kanji = '山';
-    let ids = "山山"
-    
+  public restore(subIds: string[], way:OperationState): KanjiWithIds {
+    let kanji = '';
+    let ids: string[] = [];
+    let subtree;
+    // 下向きに復元
+    if (way == OperationState.BOTTOM) {
+      ids = subIds;
+      subtree = this.kanjiTree;
+    } else if (way == OperationState.TOP) {
+      // 上向きに復元
+      ids = subIds.reverse();
+      subtree = this.kanjiTreeReverse;
+    }
 
+    for (let i = 0; i < subIds.length; i++) {
+      subtree = subtree[subIds[i]];
+    }
+    while (true){
+      const keys : string[] = Object.keys(subtree);
+      const randomKey = keys[Math.floor(Math.random() * keys.length)];
+      const randomValue :any = subtree[randomKey];
+      if (randomKey == "terminal"){
+        kanji = randomValue;
+        break;
+      } else {
+        ids.push(randomKey);
+        subtree = randomValue;
+      }
+    }
+    if (way == OperationState.TOP) {
+      ids = ids.reverse();
+    }
     return {kanji, ids};
   }
 }

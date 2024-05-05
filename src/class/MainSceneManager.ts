f9ed269ded771fb0a419ad7b140dc1f1ea5892ea
Shusnuke Oba <obashun22@yahoo.co.jp>
2024-05-06 03:55:53 +0900

    Update 各種メソッドの処理実装 #13

--- src/class/MainSceneManager.ts
+++ src/class/MainSceneManager.ts
@@ -5,27 +5,29 @@ enum OperationState {
 }
 
 export class MainSceneManager {
-  kanjiElements: string;
-  originalElements: string;
+  kanjiElements: string[];
+  originalElements: string[];
   originalKanji: string;
   operationState: OperationState;
 
   constructor() {
-    this.kanjiElements = "⿱田";
-    this.originalElements = "⿱田力";
+    this.kanjiElements = ["⿱", "田"];
+    this.originalElements = ["⿱", "田", "力"];
     this.originalKanji = "力";
     this.operationState = OperationState.INIT;
   }
 
-  private setKanjiElements = (newElements: string): void => {
+  private setKanjiElements = (newElements: string[]): void => {
     this.kanjiElements = newElements;
   };
 
   public sharpenTop(): void {
     // 文字数のバリデーション
-    // 上から磨く処理
-    // 結果をkanjiElementsにセット
-    // ステートの更新
+    // FIXME: 正しい要件を満たすように要修正
+    if (this.kanjiElements.length <= 1) return;
+    // "上から磨く"処理
+    this.setKanjiElements(this.kanjiElements.slice(1));
+    // 操作ステートの更新
     this.operationState = OperationState.TOP;
   }
 
@@ -38,14 +40,18 @@ export class MainSceneManager {
 
   public sharpenBottom(): void {
     // 文字数のバリデーション
+    // FIXME: 正しい要件を満たすように要修正
+    if (this.kanjiElements.length <= 1) return;
     // 下から磨く処理
-    // 結果をkanjiElementsにセット
-    // ステートの更新
+    this.setKanjiElements(this.kanjiElements.slice(0, -1));
+    // 操作ステートの更新
     this.operationState = OperationState.BOTTOM;
   }
 
   public complete(): void {
-    // もしoriginalKanjiとkanjiElementsが一致していたらエンディングシーンに遷移
+    // 復元直後、即ち操作ステートがINIT以外なら早期リターン
+    if (this.operationState !== OperationState.INIT) return;
+
     // スコアを計算して諸々の情報と共にエンディングシーンに渡す
   }
 }
